a1d14a1f1922283f2e73ca285e7587ff6e1531db
StuartLiam <atlestuart@gmail.com>
2021-05-28 12:36:26 -0400

    first attempt

---
+++ arc_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/mute/workspace/arc_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/mute/workspace/arc_ws/devel;/opt/ros/kinetic".split(';') if "/home/mute/workspace/arc_ws/devel;/opt/ros/kinetic" != "" else []
